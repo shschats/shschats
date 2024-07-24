608f900bdc43d911f3f50227e096484898e7664f
shschats <shschats@gmail.com>
2024-07-23 20:37:29 -0500

    a

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -5,8 +5,6 @@ export async function POST(req: Request) {
   console.log('got it')
   try {
     const formData = await req.formData();
-    let body = Object.fromEntries(formData);
-    /*
     const author = formData.get('author')
     const authorEmail = formData.get('authorEmail')
 
@@ -46,7 +44,7 @@ export async function POST(req: Request) {
       postAttachments: attachments,
     })
     newPost.save()
-    */
+    
     return new NextResponse(JSON.stringify('success'), { status: 200 });
   } catch(e: any) {
     console.error('Error:', e);
