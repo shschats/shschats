60caddf990f661aaa5d24f691d38fb7211a3de92
shschats <shschats@gmail.com>
2024-07-23 21:03:18 -0500

    b

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,12 +1,6 @@
 import { ChatsModel } from "@/app/lib/Schemas";
 import { NextResponse } from "next/server";
 
-export const config = {
-  api: {
-      bodyParser: false,
-  },
-};
-
 export async function POST(req: Request) {
   console.log('got it');
   try {
