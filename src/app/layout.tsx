4e652c6a49d9b5e4f48496958b61f49aa70f0139
shschats <shschats@gmail.com>
2024-07-23 22:33:27 -0500

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { getServerSession } from "next-auth";
 import SessionProvider from './components/SessionProviders'
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +19,6 @@ export default async function RootLayout({
   const session = await getServerSession()
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <meta charSet="utf-8" />
-      </Head>
       <body className={inter.className}>
         <SessionProvider session={session}>
           {children}
